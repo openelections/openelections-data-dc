9baad1ec214ae4ebc0df069a0fd4a3cf3481b7f8
LindsayYoung <lindsayn.young@gsa.gov>
2016-03-13 16:43:29 -0600

    Lower casing shadow senator, it was not a required office, but it was just as easy to add it so I did.

--- precinct_script/process_DC_raw.py
+++ precinct_script/process_DC_raw.py
@@ -3,7 +3,6 @@ import re
 
 results = []
 precinct_ward_mapping = {}
-office_district_map = {'president': '', 'delegate': 'At Large'}
 
 precint_ward_file = 'precinct_ward_mapping_2002_2011.csv'
 output_file = '../2004/20041102__dc__general__precinct.csv'
@@ -21,21 +20,41 @@ def parse_record(row, precinct, office):
     Keeps track of the information that is stored as headings
     by updating the variable. Adds vote tally to results.
     """
-    if 'Total' in row[0] or 'Registration' in row[0] or 'Turnout' in row[0]:
+    if 'Registration' in row[0] or 'Turnout' in row[0]:
         pass
     elif 'Precinct' in row[0]:
         precinct = re.findall('\d+', row[0])[0]
+        # turnout is listed first, this filters that out
+        office = 'turnout'
     elif 'PRESIDENT' in row[0]:
         office = 'president'
     elif 'DELEGATE' in row[0]:
         office = 'delegate'
+    elif 'MEMBER OF THE COUNCIL' in row[0]:
+        office = 'council'
+    elif 'BOARD OF EDU' in row[0]:
+        office = 'board of education'
+    elif 'UNITED STATES REPRESENTATIVE' in row[0]:
+        office = 'shadow senator'
     else:
-        if 'Write In' in row[0]:
-            party = ''
-            candidate = 'Write in'
-        else:
+        if office == 'turnout':
+            return (precinct, office)
+        if '-' in row[0]:
             candidate = row[0][5:]
             party = row[0][:3]
+        elif 'Total' in row[0]:
+            candidate = 'Total'
+            party = ''
+        elif 'Write In' in row[0]:
+            candidate = 'Write in'
+            party = ''
+        else:
+            candidate = row[0]
+            party = ''
+        if office == 'delegate':
+            district = 'At Large'
+        else:
+            district = ''
         results.append({
             'precinct': precinct,
             'office': office,
@@ -43,7 +62,7 @@ def parse_record(row, precinct, office):
             'party': party,
             'votes': row[1],
             'ward': precinct_ward_mapping[precinct],
-            'district': office_district_map[office],
+            'district': district,
         })
         return (precinct, office)
     return (precinct, office)
@@ -59,8 +78,12 @@ with open("DC_RAW.csv", "r") as csvfile:
 
 
 with open(output_file, 'w') as csvfile:
-    fieldnames = ['ward', 'precinct', 'office', 'district', 'party', 'candidate', 'votes']
+    fieldnames = [
+        'ward', 'precinct', 'office', 'district',
+        'party', 'candidate', 'votes',
+    ]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
+
     for r in results:
         writer.writerow(r)
